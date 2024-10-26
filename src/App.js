9cdb834eb2445a5ccd13978a10ea04b5c147e0f0
sairam116 <sairamparasa116@gmail.com>
2024-10-26 11:26:59 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Users from './components/Users';
+import './styles.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+
+const App = () => {
+    return (
+        <Router>
+            <nav>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/users">Users</Link></li>
+                </ul>
+            </nav>
+
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/users" element={<Users />} />
+            </Routes>
+        </Router>
+    );
+};
+
+export default App;
